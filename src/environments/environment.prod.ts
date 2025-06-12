ca2d30e810cece38c6b9c7b8a079fae8680b78f4
bharathRajaSL <bharathr@shikshalokam.org>
2025-06-12 20:02:04 +0530

    env-prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   baseUrl:window['env' as any]['baseURL' as any]as unknown as string,
-  customAuth: window['env' as any]['customAuth' as any]as unknown as string
+  customAuth: window['env' as any]['customAuth' as any]as unknown as string,
+  stagingURl: window['env' as any]['stagingURl' as any]as unknown as string
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,9 @@
 export const environment = {
   production: false,
   baseUrl:window['env' as any]['baseURL' as any]as unknown as string,
-  customAuth: window['env' as any]['customAuth' as any]as unknown as string
+  customAuth: window['env' as any]['customAuth' as any]as unknown as string,
+  stagingURl: window['env' as any]['stagingURl' as any]as unknown as string
+
 };  
 // Update this link during deployment
 /*
