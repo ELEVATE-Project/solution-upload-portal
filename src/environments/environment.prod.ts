43865ab29779badc601e682591e46eb57be0e334
bharathRajaSL <bharathr@shikshalokam.org>
2025-06-12 20:21:48 +0530

    env case

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl:window['env' as any]['baseURL' as any]as unknown as string,
+  baseUrl:window['env' as any]['baseUrl' as any]as unknown as string,
   customAuth: window['env' as any]['customAuth' as any]as unknown as string,
   stagingURl: window['env' as any]['stagingURl' as any]as unknown as string
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 
 export const environment = {
   production: false,
-  baseUrl:window['env' as any]['baseURL' as any]as unknown as string,
+  baseUrl:window['env' as any]['baseUrl' as any]as unknown as string,
   customAuth: window['env' as any]['customAuth' as any]as unknown as string,
   stagingURl: window['env' as any]['stagingURl' as any]as unknown as string
 
-};  
+}; 
 // Update this link during deployment
 /*
  * For easier debugging in development mode, you can import the following file
