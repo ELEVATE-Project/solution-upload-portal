5e58f24dc4257f5cf2d9100b24f0b81de84a7f60
priyanka-TL <priyankapratheepan@gmail.com>
2025-07-04 16:07:20 +0530

    update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 4200;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'src/index.html'));
   });
 
 app.listen(port, () => {
