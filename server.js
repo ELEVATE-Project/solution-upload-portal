6db35874db6778d320acf030c482d48c916105c7
priyanka-TL <priyankapratheepan@gmail.com>
2025-07-04 16:15:51 +0530

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 4200;
-
+console.log(process.env.PORT, port,"ENV PORT, port")
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
